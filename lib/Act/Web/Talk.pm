5876b42ba400e047a71ff51915b9523549235da2
Sawyer X <xsawyerx@cpan.org>
2014-11-28 16:24:17 +0100

    fix this shit

--- lib/Act/Web/Talk.pm
+++ lib/Act/Web/Talk.pm
@@ -2,61 +2,57 @@ package Act::Web::Talk;
 use Dancer2 appname => 'Act::Web';
 
 # /fpw2013/**
-prefix '/:conf_id' => sub {
-    # Act::Handler::Talk::Show
-    post qr{^/talk/(\d+)$} => sub {
-        my $talk_id = param('id');
-        my $conf_id = param('conf_id');
-        my $act     = var('act');
+# Act::Handler::Talk::Show
+post qr{^/(\w+)/talk/(\d+)$} => sub {
+    my ( $conf_id, $talk_id ) = splat;
+    my $act = var('act');
 
-        my $talk = $act->find_talk({
-            conf_id => $conf_id,
-            talk_id => $talk_id,
-        });
+    my $talk = $act->find_talk({
+        conf_id => $conf_id,
+        talk_id => $talk_id,
+    });
 
-        my $new_tags = param('tags');
-        my $tags     = $talk->tags;
-        $act->add_tags_to_talk({
-            talk     => $talk,
-            conf_id  => $conf_id,
-            new_tags => $new_tags,
-        });
+    my $new_tags = param('tags');
+    my $tags     = $talk->tags;
+    $act->add_tags_to_talk({
+        talk     => $talk,
+        conf_id  => $conf_id,
+        new_tags => $new_tags,
+    });
 
-        redirect "/$conf_id/talk/$talk->id";
-    };
+    redirect "/$conf_id/talk/$talk->id";
+};
 
-    get qr{^/talk/(\d+)$} => sub {
-        my $talk_id = param('id');
-        my $conf_id = param('conf_id');
-        my $act     = var('act');
+get qr{^/(\w+)/talk/(\d+)$} => sub {
+    my ( $conf_id, $talk_id ) = splat;
+    my $act = var('act');
 
-        my $talk = $act->find_talk({
-            conf_id => $conf_id,
-            talk_id => $talk_id,
-        });
+    my $talk = $act->find_talk({
+        conf_id => $conf_id,
+        talk_id => $talk_id,
+    });
 
-        # XXX this is actually speaker, not "user"
-        # XXX should we really return a 404? better redirect + flash msg
-        $talk->displayable && $talk->presenters
-            or send_error(404);
+    # XXX this is actually speaker, not "user"
+    # XXX should we really return a 404? better redirect + flash msg
+    $talk->displayable && $talk->presenters
+        or send_error(404);
 
-        my $template = Act::Template::HTML->new();
-        $template->variables(
-            chunked_abstract => Act::Abstract::chunked( $talk->abstract ),
-            talk => $talk,
-            user => var('user'),
+    my $template = Act::Template::HTML->new();
+    $template->variables(
+        chunked_abstract => Act::Abstract::chunked( $talk->abstract ),
+        talk => $talk,
+        user => var('user'),
 
-            # 'attendees' and 'tags' are derived from 'talk' object
-        );
+        # 'attendees' and 'tags' are derived from 'talk' object
+    );
 
-        # talk level?!
+    # talk level?!
 
-        return $template->process('talk/show');
-    };
+    return $template->process('talk/show');
+};
 
-    # Act::Handler::Talk::List
-    get '/talks' => sub {
-    };
+# Act::Handler::Talk::List
+get '/talks' => sub {
 };
 
 1;
