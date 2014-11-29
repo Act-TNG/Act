61b8fe5bf064e0c1df9f764d77b4878c21cb1679
Sawyer X <xsawyerx@cpan.org>
2014-11-29 12:41:01 +0100

    add routes for an event

--- lib/Act/Web/Event.pm
+++ lib/Act/Web/Event.pm
@@ -1,5 +1,46 @@
 package Act::Web::Event;
 use Dancer2 appname => 'Act::Web';
 
+# originally: list
+get '/events' => sub {
+    # we require act to even match
+    my $conf_id = param('conf_id') or pass;
+
+    ...
+    $template->process('event/list');
+};
+
+# or: /event/:id
+get '/event' => sub {
+    # we require act to even match
+    my $conf_id = param('conf_id') or pass;
+    my $talk_id = param('talk_id') or pass;
+
+    $template->process('event/show');
+};
+
+# or: /event/edit
+get '/editevent' => sub {
+    # we require act to even match
+    my $conf_id = param('conf_id') or pass;
+    my $talk_id = param('talk_id') or pass;
+
+    my $return_url = param('return_url') ||
+        request->referer if request->referer =~ m{/schedule};
+
+    $template->process('event/add');
+};
+
+post '/editevent' => sub {
+    # we require act to even match
+    my $conf_id = param('conf_id') or pass;
+    my $talk_id = param('talk_id') or pass;
+
+    my $return_url = param('return_url') ||
+        request->referer if request->referer =~ m{/schedule};
+
+    redirect "/$conf_id/editevent?talk_id=$talk_id";
+};
+
 1;
 
