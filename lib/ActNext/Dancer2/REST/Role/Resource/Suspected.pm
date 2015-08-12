13987538cac1f579cda1e1a25dd5665fd04be4e9
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2015-08-12 23:47:00 +0100

    changed all the Packages to reflect ActNext

---
+++ lib/ActNext/Dancer2/REST/Role/Resource/Suspected.pm
@@ -0,0 +1,17 @@
+package ActNext::Dancer2::REST::Role::Resource::Suspected;
+
+=head1 NAME
+
+ActNext::Dancer2::REST::Role::Resource::Suspected
+
+The C<Suspected> role provides an interface for all ClientUsers that provided
+bad credentials.
+
+=cut
+
+use Moo::Role;
+
+# methods will get added for clients that can not authenticate
+
+1;
+
